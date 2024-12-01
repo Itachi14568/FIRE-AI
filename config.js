cd8f0fc24120ae935866efe41c7b83f0ae30f76d
Itachi14568 <alexanderbrown2789@gmail.com>
2024-12-01 20:31:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2349013432338', '2349013432338']
+global.mods = ['2349043546856']
 global.prems = ['2349013432338', '2349013432338', '2349013432338']
 global.allowed = ['2349013432338', '2349013432338', '2349013432338']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
@@ -70,9 +70,9 @@ global.APIKeys = {
 
 // Sticker WM
 global.botname = process.env.BOTNAME
-global.premium = 'false'
+global.premium = 'true'
 global.packname = 'FIRE MD'
-global.author = 'OPAKSFIRE'
+global.author = '下K・ÎTÃÇHÎ'
 global.menuvid = 'https://files.catbox.moe/r9oil6.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/\n'
 global.dygp = 'https://whatsapp.com/channel/0029VajYjMJJf05aRQXKx82W'
